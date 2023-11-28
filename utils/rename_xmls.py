36140bcc312f92a1528254031c0f83439bfa6a5f
syntaxticsugr <132896073+syntaxticsugr@users.noreply.github.com>
2023-11-28 23:04:03 +0530

    sugr

--- utils/rename_xmls.py
+++ utils/rename_xmls.py
@@ -1,37 +1,38 @@
 import os
 
-# Path to Preset folder
+
+
+# Function to Rename Preset XMLs
+def rename_xmls(preset_folder, renamed_xml_directory):
+    # Getting the list of folders within Preset folder
+    folders = os.listdir(preset_folder)
+
+    # Accessing each folder
+    for folder in folders:
+
+        # Getting list of files in folder
+        # Here only one file in each folder in most cases
+        files = os.listdir(r'{}/{}'.format(preset_folder, folder))
+
+        # Accessing each file
+        for file in files:
+
+            # Getting the file extension
+            _, extension = os.path.splitext(file)
+
+            # Renaming file to folder name
+            # Changing "_" and " " to "-" in XMLs Name
+            # And placing them in new Renamed XML Directory
+            os.rename(r'{}/{}/{}'.format(preset_folder, folder, file),
+                    r'{}/{}{}'.format(renamed_xml_directory, folder.strip().replace("_", "-").replace(" ", "-"), extension))
+
+
+
+# Path to XMLs folder
 preset_folder = r''
 
-# Path to New Directory for storing Renamed Presets
+# Path to a new folder for saving Renamed XMLs
 renamed_xml_directory = r''
 
-# Getting the list of folders within Preset folder
-folders = os.listdir(preset_folder)
-
-# Total Number of XML Folders 
-count = len(folders)
-
-# Accessing each folder
-for folder in folders:
-
-    # Getting list of files in folder
-    # Only one file in each folder in this case
-    files = os.listdir(r'{}/{}'.format(preset_folder, folder))
-
-    # Accessing each file
-    for file in files:
-
-        # Getting the file extension
-        extension_pos = file.rfind(".")
-        extension = file[extension_pos:]
-
-        # Renaming file to folder name
-        # Changing "_" and " " to "-" in XML's Name
-        # And placing them in new Renamed XML Directory
-        os.rename(r'{}/{}/{}'.format(preset_folder, folder, file),
-                  r'{}/{}{}'.format(renamed_xml_directory, folder.strip().replace("_", "-").replace(" ", "-"), extension))
-
-    # Printing Number of Remaining Folders to Process
-    count-=1
-    print("Remaining: ", count)
+# Function Call
+rename_xmls(preset_folder, renamed_xml_directory)
