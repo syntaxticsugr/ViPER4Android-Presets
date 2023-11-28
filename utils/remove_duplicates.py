da0f6e83d37e0c6b2bc0cc3fd723e9a38bdd3375
syntaxticsugr <132896073+syntaxticsugr@users.noreply.github.com>
2023-11-28 23:09:56 +0530

    sugr; slight improvement in overall logic

--- utils/remove_duplicates.py
+++ utils/remove_duplicates.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
 Fast duplicate file finder.
 Usage: duplicates.py <folder> [<folder>...]
@@ -6,15 +5,9 @@ Based on https://stackoverflow.com/a/36113168/300783
 Modified for Python3 with some small code improvements.
 """
 import os
-import sys
 import hashlib
 from collections import defaultdict
 
-
-# Path to Text Files
-dup_vdc_txt_file = r''
-dup_irs_txt_file = r''
-dup_xml_txt_file = r''
 
 
 def chunk_reader(fobj, chunk_size=1024):
@@ -24,6 +17,7 @@ def chunk_reader(fobj, chunk_size=1024):
         if not chunk:
             return
         yield chunk
+
 
 
 def get_hash(filename, first_chunk_only=False, hash_algo=hashlib.sha1):
@@ -37,24 +31,24 @@ def get_hash(filename, first_chunk_only=False, hash_algo=hashlib.sha1):
     return hashobj.digest()
 
 
-def check_for_duplicates(paths):
+
+def check_for_duplicates(path):
     files_by_size = defaultdict(list)
     files_by_small_hash = defaultdict(list)
-    files_by_full_hash = dict()
+    files_by_full_hash = defaultdict(list)
 
-    for path in paths:
-        for dirpath, _, filenames in os.walk(path):
-            for filename in filenames:
-                full_path = os.path.join(dirpath, filename)
-                try:
-                    # if the target is a symlink (soft one), this will
-                    # dereference it - change the value to the actual target file
-                    full_path = os.path.realpath(full_path)
-                    file_size = os.path.getsize(full_path)
-                except OSError:
-                    # not accessible (permissions, etc) - pass on
-                    continue
-                files_by_size[file_size].append(full_path)
+    for dirpath, _, filenames in os.walk(path):
+        for filename in filenames:
+            full_path = os.path.join(dirpath, filename)
+            try:
+                # if the target is a symlink (soft one), this will
+                # dereference it - change the value to the actual target file
+                full_path = os.path.realpath(full_path)
+                file_size = os.path.getsize(full_path)
+            except OSError:
+                # not accessible (permissions, etc) - pass on
+                continue
+            files_by_size[file_size].append(full_path)
 
     # For all files with the same file size, get their hash on the first 1024 bytes
     for file_size, files in files_by_size.items():
@@ -69,63 +63,70 @@ def check_for_duplicates(paths):
                 continue
             files_by_small_hash[(file_size, small_hash)].append(filename)
 
-    # Opening Files To Work Upon
-    # dup_ddc.txt
-    # dup_irs.txt
-    # dup_xml.txt
-    # Collecting Duplicate File Names In Respective Text File
-    with open(dup_vdc_txt_file, 'w+') as dup_vdc_txt, open(dup_irs_txt_file, 'w+') as dup_irs_txt, open(dup_xml_txt_file, 'w+') as dup_xml_txt:
-        # For all files with the hash on the first 1024 bytes, get their hash on the full
-        # file - collisions will be duplicates
-        for files in files_by_small_hash.values():
-            if len(files) < 2:
-                # the hash of the first 1k bytes is unique -> skip this file
+    # For all files with the hash on the first 1024 bytes, get their hash on the full
+    # file - collisions will be duplicates
+    for files in files_by_small_hash.values():
+        if len(files) < 2:
+            # the hash of the first 1k bytes is unique -> skip this file
+            continue
+
+        for filename in files:
+            try:
+                full_hash = get_hash(filename, first_chunk_only=False)
+            except OSError:
+                # the file access might've changed till the exec point got here
                 continue
 
-            for filename in files:
-                try:
-                    full_hash = get_hash(filename, first_chunk_only=False)
-                except OSError:
-                    # the file access might've changed till the exec point got here
-                    continue
+            files_by_full_hash[full_hash].append(filename)
 
-                if full_hash in files_by_full_hash:
-                    duplicate = files_by_full_hash[full_hash]
-
-                    print("Duplicate found:\n - %s\n - %s\n" % (filename, duplicate))
-
-                    if (".vdc" in os.path.basename(filename)):
-                        # Collecting Duplicate File Names In Respective Text File
-                        dup_vdc_txt.write("{}\n{}\n\n".format(os.path.basename(filename), os.path.basename(duplicate)))
-                        # Deleting Duplicate VDC's
-                        try:
-                            os.remove(filename)
-                        except: pass
-
-                    if (".irs" in os.path.basename(filename)):
-                        # Collecting Duplicate File Names In Respective Text File
-                        dup_irs_txt.write("{}\n{}\n\n".format(os.path.basename(filename), os.path.basename(duplicate)))
-                        # Deleting Duplicate IRS's
-                        try:
-                            os.remove(filename)
-                        except: pass
-
-                    if (".xml" in os.path.basename(filename)):
-                        # Collecting Duplicate File Names In Respective Text File
-                        dup_xml_txt.write("{}\n{}\n\n".format(os.path.basename(filename), os.path.basename(duplicate)))
-                        # Deleting Duplicate XML's
-                        try:
-                            os.remove(filename)
-                        except: pass
-
-                else:
-                    files_by_full_hash[full_hash] = filename
+    return files_by_full_hash
 
 
-# if __name__ == "__main__":
-#     if sys.argv[1:]:
-#         check_for_duplicates(sys.argv[1:])
-#     else:
-#         print("Usage: %s <folder> [<folder>...]" % sys.argv[0])
 
-check_for_duplicates([])
+# Function to Write Duplicate File Names in Respective Text Files
+def save_duplicate_names(folder_path, dup_txt_file):
+    files_by_full_hash = check_for_duplicates(folder_path)
+    duplicate_files = {k: v for k, v in files_by_full_hash.items() if len(v) > 1}
+    with open(dup_txt_file, 'w+', encoding='utf-8') as dup_txt:
+        for hash_value, files in duplicate_files.items():
+            dup_txt.write(f"Hash: {hash_value}\n")
+            for file_path in files:
+                dup_txt.write(f"  - {file_path}\n")
+            dup_txt.write("\n")
+
+
+
+# Function to Save Unique Files in a different folder
+def save_unique_files(folder_path, new_save_path):
+    files_by_full_hash = check_for_duplicates(folder_path)
+    for _, files in files_by_full_hash.items():
+        for file_path in files:
+            os.rename(file_path, r'{}/{}'.format(new_save_path, (file_path.split("\\"))[-1]))
+            break
+
+
+
+# Path to Text Files for saving Names of Duplicate files
+dup_vdc_txt_file = r'C:/Users/aditya/Desktop/New folder/utils/dup_vdc.txt'
+dup_irs_txt_file = r'C:/Users/aditya/Desktop/New folder/utils/dup_irs.txt'
+dup_xml_txt_file = r'C:/Users/aditya/Desktop/New folder/utils/dup_xml.txt'
+
+# Path to Folders where need to check Duplicates
+vdc_path = r'C:/Users/aditya/Desktop/New folder/DDC'
+irs_path = r'C:/Users/aditya/Desktop/New folder/Kernel'
+xml_path = r'C:/Users/aditya/Desktop/New folder/XML-Converted'
+
+# Path to Folder where to save Unique files
+new_vdc_path = r'C:/Users/aditya/Desktop/New folder/DDC-Unique'
+new_irs_path = r'C:/Users/aditya/Desktop/New folder/Kernel-Unique'
+new_xml_path = r'C:/Users/aditya/Desktop/New folder/XML-Unique'
+
+# Function Call
+save_duplicate_names(vdc_path, dup_vdc_txt_file)
+save_duplicate_names(irs_path, dup_irs_txt_file)
+save_duplicate_names(xml_path, dup_xml_txt_file)
+
+# Function Call
+save_unique_files(vdc_path, new_vdc_path)
+save_unique_files(irs_path, new_irs_path)
+save_unique_files(xml_path, new_xml_path)
