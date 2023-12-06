5ecffe55bf0711134dd5035a3656e189af795bd7
syntaxticsugr <132896073+syntaxticsugr@users.noreply.github.com>
2023-12-06 10:34:23 +0530

    improved/fixed preset renaming script

--- utils/rename_presets.py
+++ utils/rename_presets.py
@@ -1,5 +1,13 @@
 import os
+import pathlib
+import re
 import shutil
+
+
+
+def regex_replace(string, replace_dict):
+    new_string = re.sub("|".join(replace_dict.keys()), lambda match: replace_dict[match.string[match.start():match.end()]], string)
+    return new_string
 
 
 
@@ -9,7 +17,7 @@ def rename_presets(presets_folder, renamed_presets_folder):
     # Getting the list of subfolders within Presets folder
     sub_folders = os.listdir(presets_folder)
 
-    # Total number of subfolders in Presets folder 
+    # Total number of subfolders in Presets folder
     count = len(sub_folders)
 
     # Accessing each subfolder
@@ -17,20 +25,35 @@ def rename_presets(presets_folder, renamed_presets_folder):
 
         # Getting list of files in subfolder
         # Only one file in each subfolder in most cases
-        files = os.listdir(r'{}/{}'.format(presets_folder, sub_folder))
+        files = os.listdir(f'{presets_folder}/{sub_folder}')
 
         # Accessing each file in subfolder
         for file in files:
 
-            # Renaming file to subfolder name
-            # Changing "_" and " " to "-" in Presets Name
+            # Renaming Presets
             # Placing renamed Presets in renamed-presets folder
-            source = r'{}/{}/{}'.format(presets_folder, sub_folder, file)
-            destination = r'{}/{}.xml'.format(renamed_presets_folder, sub_folder.strip().replace("_", "-").replace(" ", "-"))
-            try:
-                shutil.copyfile(source, destination)
-            except:
-                pass
+            preset_old_name = pathlib.Path(file).stem
+            replace_dict = {
+                "_" : "-",
+                " " : "-",
+                "headset" : "",
+                "bluetooth" : "",
+                "usb" : "",
+                "speaker" : ""
+            }
+            preset_new_name = regex_replace(sub_folder, replace_dict)
+            preset_new_name = f"{preset_new_name}-{preset_old_name}"
+            replace_dict = {
+                "--" : "-",
+                "bt_a2dp" : "bluetooth",
+                "usb_device" : "usb"
+            }
+            preset_new_name = regex_replace(preset_new_name, replace_dict)
+
+            source = f'{presets_folder}/{sub_folder}/{file}'
+            destination = f'{renamed_presets_folder}/{preset_new_name}.xml'
+
+            shutil.copyfile(source, destination)
 
         # Printing number of remaining subfolders to process
         count-=1
