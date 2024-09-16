d66aa33c16b2314ab269bb3265e6b823cbe09578
syntaxticsugr <syntaxticsugr@gmail.com>
2024-09-17 01:56:55 +0530

    convert script

--- scripts/main.py
+++ scripts/main.py
@@ -3,18 +3,21 @@ import sys
 from pathlib import Path
 from utils.extract import extract
 from utils.filter import filter_irs_vdc
+from utils.convert import convert_presets
 
 
 
-def process_directory(input_dir: Path, extract_dir: Path, filter_dir: Path):
+def process_directory(input_dir: Path, extract_dir: Path, filter_dir: Path, preset_converted_dir: Path):
 
     extract(input_dir, extract_dir)
 
     filter_irs_vdc(extract_dir, filter_dir)
 
+    convert_presets(f"{filter_dir}/Preset", preset_converted_dir)
 
 
-def main(input_dir: Path, extract_dir: Path, filter_dir: Path):
+
+def main(input_dir: Path, extract_dir: Path, filter_dir: Path, preset_converted_dir: Path):
 
     if not os.path.isdir(input_dir):
         print(f"Error: The input directory '{input_dir}' does not exist.")
@@ -22,8 +25,9 @@ def main(input_dir: Path, extract_dir: Path, filter_dir: Path):
 
     extract_dir.mkdir(parents=True, exist_ok=True)
     filter_dir.mkdir(parents=True, exist_ok=True)
+    preset_converted_dir.mkdir(parents=True, exist_ok=True)
 
-    process_directory(input_dir, extract_dir, filter_dir)
+    process_directory(input_dir, extract_dir, filter_dir, preset_converted_dir)
 
 
 
@@ -32,5 +36,6 @@ if __name__ == "__main__":
     input_dir = Path('in')
     extract_dir = Path('out/extracted')
     filter_dir = Path('out/filtered')
+    preset_converted_dir = Path('out/filtered/Preset-Converted')
 
-    main(input_dir, extract_dir, filter_dir)
+    main(input_dir, extract_dir, filter_dir, preset_converted_dir)
