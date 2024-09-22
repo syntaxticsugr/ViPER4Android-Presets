911f6a368671c956355b3226e7511ba12843f8ad
syntaxticsugr <syntaxticsugr@gmail.com>
2024-09-22 21:53:42 +0530

    update output path

--- scripts/main.py
+++ scripts/main.py
@@ -47,8 +47,8 @@ def main(input_dir: Path, extract_dir: Path, filter_dir: Path, preset_converted_
 if __name__ == "__main__":
 
     input_dir = Path('in')
-    extract_dir = Path('out/extracted')
-    filter_dir = Path('out/filtered')
-    preset_converted_dir = Path('out/preset-converted')
+    extract_dir = Path('build/output/extracted')
+    filter_dir = Path('build/output/filtered')
+    preset_converted_dir = Path('build/output/preset-converted')
 
     main(input_dir, extract_dir, filter_dir, preset_converted_dir)
