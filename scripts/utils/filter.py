2bad02ccca7f32fb0028013628b093bbb8fbf862
syntaxticsugr <syntaxticsugr@gmail.com>
2024-09-22 21:56:45 +0530

    revert changing filenames to lowercase & handle filenames with exact character sequence but different cases

--- scripts/utils/filter.py
+++ scripts/utils/filter.py
@@ -6,17 +6,9 @@ from pathlib import Path
 
 
 
-def get_new_suffix(path: Path, suffix=2):
-    if (Path(f'{path}_{suffix}').is_file()):
-        get_new_suffix(path, suffix=(suffix+1))
-    else:
-        return suffix
-
-
-
-def sha256(file_path):
+def sha256(root):
     hasher = hashlib.sha256()
-    with open(file_path, 'rb') as f:
+    with open(root, 'rb') as f:
         for chunk in iter(lambda: f.read(1024), b''):
             hasher.update(chunk)
     return hasher.hexdigest()
@@ -25,53 +17,67 @@ def sha256(file_path):
 
 def filter_irs_vdc_xml(input_dir: Path, filter_dir: Path, irs_dir: Path, vdc_dir: Path, xml_dir: Path):
 
+    IRSs = defaultdict(int)
+    VDCs = defaultdict(int)
+    XMLs = defaultdict(int)
+
     irs_hashes = defaultdict(set)
     vdc_hashes = defaultdict(set)
     xml_hashes = defaultdict(set)
 
     for root, _, files in os.walk(input_dir):
+        root = Path(root)
 
         for file in files:
-            file_path = Path(root)
-            full_path = file_path/file
+            full_path = root/file
 
-            file_name = full_path.stem.lower()
+            file_name = new_file_name = full_path.stem
             file_extension = full_path.suffix
 
+            file_name_lower = full_path.stem.lower()
             file_hash = sha256(full_path)
 
             if (file_extension == '.irs'):
-                if (file_name not in irs_hashes[file_hash]):
-                    irs_hashes[file_hash].add(file_name)
+                if (file_name_lower not in irs_hashes[file_hash]):
+                    irs_hashes[file_hash].add(file_name_lower)
 
-                    filtered_irs = Path(f'{irs_dir/file_name}.irs')
-                    if (filtered_irs.is_file()):
-                        file_name = f'{file_name}_{get_new_suffix(filtered_irs)}'
+                    name_repeat_count = IRSs[file_name_lower]
+                    name_repeat_count += 1
+                    IRSs[file_name_lower] = name_repeat_count
 
-                    shutil.copy2(full_path, f'{irs_dir/file_name}.irs')
+                    if (1 < name_repeat_count):
+                        new_file_name = f'{file_name}_{name_repeat_count}'
+
+                    shutil.copy2(full_path, f'{irs_dir/new_file_name}.irs')
 
             elif (file_extension == '.vdc'):
-                if (file_name not in vdc_hashes[file_hash]):
-                    vdc_hashes[file_hash].add(file_name)
+                if (file_name_lower not in vdc_hashes[file_hash]):
+                    vdc_hashes[file_hash].add(file_name_lower)
 
-                    filtered_vdc = Path(f'{vdc_dir/file_name}.vdc')
-                    if (filtered_vdc.is_file()):
-                        file_name = f'{file_name}_{get_new_suffix(filtered_vdc)}'
+                    name_repeat_count = VDCs[file_name_lower]
+                    name_repeat_count += 1
+                    VDCs[file_name_lower] = name_repeat_count
 
-                    shutil.copy2(full_path, f'{vdc_dir/file_name}.vdc')
+                    if (1 < name_repeat_count):
+                        new_file_name = f'{file_name}_{name_repeat_count}'
+
+                    shutil.copy2(full_path, f'{vdc_dir/new_file_name}.vdc')
 
             elif (file_extension == '.xml'):
-                if (file_name in ['bt_a2dp', 'headset', 'speaker', 'usb_device']):
-                    file_name = f'{file_path.stem}{file_path.suffix}-{file_name}'.lower()
+                if (file_name_lower in ['bt_a2dp', 'headset', 'speaker', 'usb_device']):
+                    new_file_name = f'{root.stem}{root.suffix}-{file_name_lower}'.lower()
 
-                if (file_name not in xml_hashes[file_hash]):
-                    xml_hashes[file_hash].add(file_name)
+                if (new_file_name not in xml_hashes[file_hash]):
+                    xml_hashes[file_hash].add(new_file_name)
 
-                    filtered_xml = Path(f'{xml_dir/file_name}.xml')
-                    if (filtered_xml.is_file()):
-                        file_name = f'{file_name}_{get_new_suffix(filtered_xml)}'
+                    name_repeat_count = XMLs[new_file_name]
+                    name_repeat_count += 1
+                    XMLs[new_file_name] = name_repeat_count
 
-                    shutil.copy2(full_path, f'{xml_dir/file_name}.xml')
+                    if (1 < name_repeat_count):
+                        new_file_name = f'{new_file_name}_{name_repeat_count}'
+
+                    shutil.copy2(full_path, f'{xml_dir/new_file_name}.xml')
 
     with(
         open(filter_dir/'irs_dup.txt', 'w') as irs_dup_txt,
@@ -91,9 +97,9 @@ def filter_irs_vdc_xml(input_dir: Path, filter_dir: Path, irs_dir: Path, vdc_dir
         for key, value in xml_hashes.items():
             xml_dup.append(f'{len(value)} : {key} : {value}\n')
 
-        irs_dup.sort()
-        vdc_dup.sort()
-        xml_dup.sort()
+        irs_dup = sorted(irs_dup, key=lambda x: (int(x.split(' : ')[0]), x.split(' : ')[1]), reverse=True)
+        vdc_dup = sorted(vdc_dup, key=lambda x: (int(x.split(' : ')[0]), x.split(' : ')[1]), reverse=True)
+        xml_dup = sorted(xml_dup, key=lambda x: (int(x.split(' : ')[0]), x.split(' : ')[1]), reverse=True)
 
         irs_dup_txt.writelines(irs_dup)
         vdc_dup_txt.writelines(vdc_dup)
