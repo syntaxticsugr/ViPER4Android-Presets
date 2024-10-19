46321c67ce44014945ba46dc08f39fe17c72a3d9
syntaxticsugr <syntaxticsugr@gmail.com>
2024-10-19 16:25:01 +0530

    syntactic sugar

--- scripts/utils/filter.py
+++ scripts/utils/filter.py
@@ -2,9 +2,39 @@ import os
 import shutil
 from collections import defaultdict
 from pathlib import Path
-from utils.check_viper_xml import check_viper_xml
 from utils.create_directories import create_directories
 from utils.sha256 import sha256
+
+
+
+def check_xml(xml_path: Path):
+
+    with open(xml_path, 'r') as xml_file:
+        xml_data = [line.strip() for line in xml_file.readlines()]
+
+        # Master Switch
+        check_features = ['<boolean name="36868"']
+
+        check_result = all(any(feature in xml_line for xml_line in xml_data) for feature in check_features)
+
+    return check_result
+
+
+
+def copy_file(full_path: Path, target_dir: Path, file_name: str, file_extension: str, hashes: defaultdict[set], counts: defaultdict[int]):
+
+    file_hash = sha256(full_path)
+
+    if (file_name not in hashes[file_hash]):
+        hashes[file_hash].add(file_name)
+
+        name_repeat_count = counts[file_name] + 1
+        counts[file_name] = name_repeat_count
+
+        if (1 < name_repeat_count):
+            file_name = f'{file_name}_{name_repeat_count}'
+
+        shutil.copy2(full_path, f'{target_dir/file_name}{file_extension}')
 
 
 
@@ -14,7 +44,6 @@ def filter_irs_vdc_xml(input_dir: Path, output_dir: Path):
     irs_dir = filter_dir/'irs'
     vdc_dir = filter_dir/'vdc'
     xml_dir = filter_dir/'xml'
-
     create_directories([filter_dir, irs_dir, vdc_dir, xml_dir])
 
     irs_hashes = defaultdict(set)
@@ -31,40 +60,19 @@ def filter_irs_vdc_xml(input_dir: Path, output_dir: Path):
         for file in files:
             full_path = root/file
 
-            file_name = new_file_name = full_path.stem
+            file_name = full_path.stem.strip()
             file_extension = full_path.suffix
 
-            file_hash = sha256(full_path)
-
             if (file_extension == '.irs'):
-                if (file_name not in irs_hashes[file_hash]):
-                    irs_hashes[file_hash].add(file_name)
-
-                    name_repeat_count = irs_counts[file_name]
-                    name_repeat_count += 1
-                    irs_counts[file_name] = name_repeat_count
-
-                    if (1 < name_repeat_count):
-                        new_file_name = f'{file_name}_{name_repeat_count}'
-
-                    shutil.copy2(full_path, f'{irs_dir/new_file_name}.irs')
+                copy_file(full_path, irs_dir, file_name, file_extension, irs_hashes, irs_counts)
 
             elif (file_extension == '.vdc'):
-                if (file_name not in vdc_hashes[file_hash]):
-                    vdc_hashes[file_hash].add(file_name)
-
-                    name_repeat_count = vdc_counts[file_name]
-                    name_repeat_count += 1
-                    vdc_counts[file_name] = name_repeat_count
-
-                    if (1 < name_repeat_count):
-                        new_file_name = f'{file_name}_{name_repeat_count}'
-
-                    shutil.copy2(full_path, f'{vdc_dir/new_file_name}.vdc')
+                copy_file(full_path, vdc_dir, file_name, file_extension, vdc_hashes, vdc_counts)
 
             elif (file_extension == '.xml'):
 
-                check_result = check_viper_xml(full_path)
+                check_result = check_xml(full_path)
+
                 if (not check_result):
                     print(f'\nXML not for ViPER:\n{full_path}')
 
@@ -75,20 +83,14 @@ def filter_irs_vdc_xml(input_dir: Path, output_dir: Path):
                         elif (file_name == 'usb_device'):
                             file_name = 'usb'
 
-                    new_file_name = f'{root.stem}{root.suffix}'.strip()
-                    if (not (any(keyword in root.stem.lower() for keyword in ['bluetooth', 'headset', 'speaker', 'usb']))):
-                        new_file_name = f'{new_file_name}-{file_name}'
+                        new_file_name = f'{root.stem}{root.suffix}'.strip()
 
-                    if (new_file_name not in xml_hashes[file_hash]):
-                        xml_hashes[file_hash].add(new_file_name)
+                        if (not (any(keyword in new_file_name.lower() for keyword in ['bluetooth', 'headset', 'speaker', 'usb']))):
+                            new_file_name = f'{new_file_name}-{file_name}'
 
-                        name_repeat_count = xml_counts[new_file_name]
-                        name_repeat_count += 1
-                        xml_counts[new_file_name] = name_repeat_count
+                    else:
+                        new_file_name = file_name
 
-                        if (1 < name_repeat_count):
-                            new_file_name = f'{new_file_name}_{name_repeat_count}'
-
-                        shutil.copy2(full_path, f'{xml_dir/new_file_name}.xml')
+                    copy_file(full_path, xml_dir, new_file_name, file_extension, xml_hashes, xml_counts)
 
     return(irs_dir, vdc_dir, xml_dir)
