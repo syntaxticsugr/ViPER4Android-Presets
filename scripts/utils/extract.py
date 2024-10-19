24b66983d6e69e21eaede2e21198d03af1f1bb75
syntaxticsugr <syntaxticsugr@gmail.com>
2024-10-19 16:30:30 +0530

    rewrite without using recursion

--- scripts/utils/extract.py
+++ scripts/utils/extract.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import patoolib
 from pathlib import Path
 from utils.create_directories import create_directories
@@ -15,44 +14,48 @@ def extract(archive_path: Path, extract_to: Path):
             verbosity = -1
         )
 
-    except:
-        print(archive_path)
+    except Exception as e:
+        print(f'Failed to extract "{archive_path}" - {e}')
 
 
 
 def extract_archives(input_dir: Path, output_dir: Path):
 
     extract_dir = output_dir/'extracted'
+    create_directories([extract_dir])
 
-    if (input_dir != output_dir):
-        create_directories([extract_dir])
+    directories_to_process = [input_dir]
 
-    for root, _, files in os.walk(input_dir):
+    while directories_to_process:
+        current_dir = directories_to_process.pop()
 
-        for file in files:
+        for root, _, files in os.walk(current_dir):
             root = Path(root)
 
-            relative_file_path = root.relative_to(input_dir)
-            full_path = root/file
+            for file in files:
+                full_path = root/file
 
-            file_name = full_path.stem
-            file_extension = full_path.suffix
-
-            if file_extension in ['.zip', '.rar', '.tar']:
-                if (input_dir != output_dir):
-                    save_dir = extract_dir/relative_file_path
-                else:
-                    save_dir = output_dir/relative_file_path/file_name
-
-                save_dir.mkdir(parents=True, exist_ok=True)
-
-                extract(full_path, save_dir)
-                extract_archives(save_dir, save_dir)
-
-            else:
                 try:
-                    shutil.copy2(full_path, extract_dir/(full_path.relative_to(input_dir)))
+                    relative_file_path = root.relative_to(input_dir)
                 except:
-                    pass
+                    relative_file_path = root.relative_to(extract_dir)
+
+                file_name = full_path.stem
+                file_extension = full_path.suffix
+
+                if file_extension in ['.zip', '.rar', '.tar']:
+
+                    save_dir = extract_dir/relative_file_path/file_name
+
+                    count = 1
+                    while (save_dir.exists()):
+                        count += 1
+                        save_dir = Path(f'{save_dir}_{count}')
+
+                    save_dir.mkdir(parents=True, exist_ok=True)
+
+                    extract(full_path, save_dir)
+
+                    directories_to_process.append(save_dir)
 
     return extract_dir
